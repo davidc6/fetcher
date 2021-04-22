da9dcb6c3588adbf371953b1ddbfab7a54c73045
David Chubabriya <david.chubabriya@gmail.com>
2021-04-22 23:45:46 +0100

    Update README and index.js

--- src/index.js
+++ src/index.js
@@ -6,15 +6,16 @@ const { createRequestConfig, left, right } = require("./components/client")
 
 const getDataForUrls = curry((config, urls) => {
   /**
-   * coalesce - bring both branches together into a whole
-   * left - failure / sad path
-   * right - success / happy path
+   * coalesce - brings both branches together into a whole
+   * https://en.wikipedia.org/wiki/Coalescing_(computer_science)
+   * left branch - failure / sad path
+   * right branch - success / happy path
    */
   const control = (urlObj) => coalesce(left(urlObj))(right(urlObj))
 
   /**
    * Function composition
-   * 
+   *
    * 1. validate urls
    * 2. create request config { url: object = {}, futures: [] = [], client: string = '' }
    * 3. prepare data structures and computations
@@ -29,9 +30,7 @@ const getDataForUrls = curry((config, urls) => {
    * parallel - runs computations
    * a Promise gets returned instead of Future to make outcome "thenable" for clients
    */
-  return promise(
-    parallel(propOr(1, "concurrencyNumber", config))(prepareRequests(urls))
-  )
+  return promise(parallel(propOr(1, "concurrencyNumber", config))(prepareRequests(urls)))
 })
 
 module.exports = {
